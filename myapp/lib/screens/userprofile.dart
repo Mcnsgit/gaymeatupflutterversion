773c068daa674c002e623e1c107862c58a43afbd
mcnsgit <cardigamiguel221@gmail.com>
2023-07-27 23:32:39 +0100

    restart:gaymeatflutterv2

--- myapp/lib/screens/userprofile.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart' show StatelessWidget;
-
-abstract class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
-  // The same ProfilePage from before goes here.
-}
